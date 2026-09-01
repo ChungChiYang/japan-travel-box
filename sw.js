0279d96911694900a3cd84d2633f26b96fff2758
ChungChiYang <93670604+ChungChiYang@users.noreply.github.com>
2026-09-01 14:12:48 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'japan-travel-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './handbook.html',
+  './database.html',
+  './communication.html'
+];
+
+// 安裝 Service Worker 並快取所有頁面
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 攔截網路請求，若無網路則從快取讀取
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
